a1d9d0b8195dcb6646319a7d89bd96001800db60
Maklakov Evgeniy <evgeniy.maklakov@gmail.com>
2020-06-08 16:37:40 +0600

    - пакет barryvdh/laravel-debugbar; - русские языковые файлы; - базовая модель App\Core\Model, от которой можно наследовать свои модели;

---
+++ app/Core/Eloquent/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core\Eloquent;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+abstract class Model extends Eloquent
+{
+	/**
+	 * @return string
+	 */
+	public static function getTableName()
+	{
+		return (new static)->getTable();
+	}
+}
--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -2,38 +2,46 @@
 
 namespace App\Models\Auth;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Core\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Model implements
+	AuthorizableContract,
+	AuthenticatableContract,
+	CanResetPasswordContract
 {
-    use Notifiable;
+	use Authenticatable, Authorizable, CanResetPassword, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 }
